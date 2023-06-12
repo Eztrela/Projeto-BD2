74d21ab0e6c3e6ada20677b371b8d3cb047ca816
Pablo de Lima Marques Estrela <102769647+Eztrela@users.noreply.github.com>
2023-06-11 22:27:24 -0300

    adicionando trigger que altera estado da roupa de acordo com o número de vezes q ela aparece em pedidos (#11)

--- Triggers.sql
+++ Triggers.sql
@@ -1,3 +1,37 @@
+
+create or replace function atualizar_estado_roupa()
+returns trigger as $$
+declare
+    quantidade_pedidos integer;
+begin
+    -- Contar a quantidade de pedidos em que a roupa aparece
+    select count(id_pedido)
+    into quantidade_pedidos
+    from pedidos_roupa
+    where id_roupa = new.id_roupa;
+    
+    -- Atualizar o estado da roupa com base na quantidade de pedidos
+    if quantidade_pedidos <= 5 then
+        update roupa set estado = 'Nova';
+    elsif quantidade_pedidos <= 10 then
+        update roupa set estado = 'Pouco Usada';
+	elsif quantidade_pedidos <= 15 then
+		update roupa set estado = 'Usada';
+	else 
+		update roupa set estado = 'Muito Usada';
+    end if;
+    
+    return null;
+end;
+$$ language plpgsql;
+
+create trigger atualizacao_estado_roupa_trigger
+after insert or update on pedidos_roupa
+for each row
+execute function atualizar_estado_roupa();
+
+select * from pedidos_roupa;
+
 /* 
 		Justificativa semântica:
 
@@ -81,4 +115,5 @@ insert into funcionario values (default,'Enzo Leclerc','495.245.234-42','2005-09
 insert into funcionario values (default,'Maria Raluca','210.810.134-94','1999-12-28','F','raluca@gmail.com',3500,2);
 
 select nome, media_salarial from setor;
+
 
