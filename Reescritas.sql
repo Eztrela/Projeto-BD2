5ec55081ebd600113c76e9a602dd695a64de7143
MatheusSousa5068 <matheus5068trab@gmail.com>
2023-06-11 22:36:11 -0300

    reescrita de subquery (#12)
    Co-authored-by: MatheusSousa5068 <matheusoficial.sousa@gmail.com>
    Co-authored-by: Pablo de Lima Marques Estrela <102769647+Eztrela@users.noreply.github.com>

--- Reescritas.sql
+++ Reescritas.sql
@@ -1,4 +1,32 @@
 /* 
+
+Podemos usar WITH para alocar
+a subconsulta interna antes realizada como: 
+
+select pl.tipo, count(*) as "Clientes" from plano pl 
+where pl.id_plano in (
+  SELECT a.id_plano from assina a
+  where a.id_cliente in (
+    select c.id_cliente from cliente c))
+ group by pl.tipo;
+
+assim, podemos melhorar a 
+leitura da consulta
+*/
+with cliente_assina as (
+  select id_plano
+  from assina
+  where id_cliente in (
+    select id_cliente
+    from cliente
+  )
+)
+
+
+select pl.tipo, count(*) as "Clientes"
+from plano pl
+join cliente_assina ca on pl.id_plano = ca.id_plano
+/*
 Podemos reescrever
 nossa consula de subqueries
 usando joins,
@@ -10,7 +38,6 @@ where pl.id_plano in (
   where a.id_cliente in (
     select c.id_cliente from cliente c))
  group by pl.tipo;
- 
 
 podemos reescrevê-lo da seguinte forma
 */
@@ -19,4 +46,5 @@ select pl.tipo, count(*) as "Clientes"
 from plano pl
 join assina a on a.id_plano = pl.id_plano
 join cliente c on c.id_cliente = a.id_cliente
+
 group by pl.tipo;
